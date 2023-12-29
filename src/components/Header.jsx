0fbe6e3e8c616c2b02db24e841e5b84daecf5a51
aymendakir <aymendakr@yahoo.com>
2023-12-29 14:14:16 +0100

    sss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,7 @@
-
 import React from "react";
 
 export default function Header() {
-    return(
-        <div className="h-[60vh] cc ">
-
-        </div>
-
-
-    )
+  return (
+    <div className="h-[60vh] bg-hero-pattern bg-cover bg-center bg-no-repeats "></div>
+  );
 }
