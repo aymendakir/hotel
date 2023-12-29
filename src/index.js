7413c66cebb09b69fc8398b3de56d922bc02620d
aymendakir <aymendakr@yahoo.com>
2023-12-29 12:55:57 +0100

    jsjsj

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element:<Home />,
-
-    },
-    {
-        path: "/Cart",
-        element:<Cart />,
-    },
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/Cart",
+    element: <Cart />,
+  },
 ]);
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
