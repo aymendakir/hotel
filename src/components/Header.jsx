9f12a7b9043781a0dd1a0fe93d38e5bc81d3742c
aymendakir <aymendakr@yahoo.com>
2024-01-12 22:07:59 +0100

    Updates

--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Header() {
-  return (
-    <div className="h-[60vh] bg-hero-pattern bg-cover bg-center bg-no-repeats w-[95%] mx-auto rounded-2xl "></div>
-  );
-}
--- src/components/Logo.jsx
+++
@@ -1,6 +0,0 @@
-import {Link} from "react-router-dom";
-
-
-export default function Logo() {
-    return <h1 className=" w-[230px] "><Link to="/"><img src={require('./img/logo.png')}/></Link></h1>;
-}
